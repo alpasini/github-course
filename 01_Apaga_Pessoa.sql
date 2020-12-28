4c365aa99e075f427791963547d8edb96cfb40e9
Alex Pasini <alpasini@gmail.com>
2020-12-28 09:39:09 -0300

    Add 01_Apaga_pessoa.sql

---
+++ 01_Apaga_Pessoa.sql
@@ -0,0 +1,19 @@
+SDYSTUR --Alex Pasini
+--Objetivo do scritp
+--Apaga pessoa cadastrada errada
+
+--Script
+begin
+    delete systur.gen_pes_fone where cd_pessoa =18019324;
+    delete systur.gen_pes_end where cd_pessoa =18019324;
+    delete systur.gen_pessoa where cd_pessoa =18019324;
+    commit;
+end;
+/
+
+
+Teste GIT
+
+Git Course
+
+
